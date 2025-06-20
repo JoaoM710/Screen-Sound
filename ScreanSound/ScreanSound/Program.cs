d8c823211bafe68a10b7be2636e6040a4c0b9623
JoaoM710 <174065055+JoaoM710@users.noreply.github.com>
2025-06-20 20:52:09 -0300

    Add files via upload

--- ScreanSound/ScreanSound/Program.cs
+++ ScreanSound/ScreanSound/Program.cs
@@ -1,15 +1,13 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.DescricaoResumida);
+﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Vertigo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = false;
+Episodio ep2 = new(2, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flavia");
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+Podcast podcast = new("Podcast especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
